ac87a7431675ff07ee3af1e8348a43bb3a0d9595
ptrocki <ptrocki@users.noreply.github.com>
2016-05-17 20:14:06 +0200

    Create sesjablocker.js

---
+++ sesjablocker.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @grant        none
+// ==/UserScript==
+
+
+(function() {
+    'use strict';
+document.documentElement.innerHTML = '';
+document.documentElement.innerHTML = '<h1>WEŹ SIĘ KURWO DO ROBOTY !!</h1></br><img src="http://67.media.tumblr.com/b45b92a8dfecda91361516a277d5d478/tumblr_o2tj9stI8W1v3elemo1_1280.jpg">' ;   
+
+})();
